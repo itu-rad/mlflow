aec8973ac994013c6214ff8b6a544beeddc4f90a
titr <titr@itu.dk>
2025-11-12 10:34:16 +0000

    rename env

--- mlflow/server/js/src/experiment-tracking/pages/radt/RadtPage.tsx
+++ mlflow/server/js/src/experiment-tracking/pages/radt/RadtPage.tsx
@@ -11,7 +11,8 @@ import { useCallback } from 'react';
 const RadtPage = () => {
   const { theme } = useDesignSystemTheme();
 
-  const DEFAULT_IFRAME_SRC = (process.env['RADT_URL'] && process.env['RADT_URL'].trim()) || 'missing_radT_url';
+  const DEFAULT_IFRAME_SRC = (process.env['REACT_APP_RADT_URL'] && process.env['REACT_APP_RADT_URL'].trim()) || 'missing_radT_url';
+  console.log(`src`, DEFAULT_IFRAME_SRC, process.env);
   const iframeRef = useRef<HTMLIFrameElement | null>(null);
   const location = useLocation();
   const navigate = useNavigate();
